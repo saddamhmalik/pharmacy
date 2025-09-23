1ecedb5e411b59a87667359f07ceb803bb16fe66
Saddam Hussain <saddamhmalik@gmail.com>
2025-09-23 13:54:17 +0530

    init

---
+++ config/pharmacy.php
@@ -0,0 +1,7 @@
+<?php
+return [
+    'name' => env('PHARMACY_NAME', 'My Pharmacy'),
+    'address' => env('PHARMACY_ADDRESS', '123 Main St, City, Country'),
+    'phone' => env('PHARMACY_PHONE', '+1234567890'),
+    'email' => env('PHARMACY_EMAIL', 'test@email.com')
+];
